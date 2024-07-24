b62340d6bfd15b391532c3c5f654e6467b69632b
Jakner <jakner@gmail.com>
2024-07-24 15:59:41 -0300

    update

--- Client/src/Routes/routes.js
+++ Client/src/Routes/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import { BrowserRouter, Route, Routes, } from "react-router-dom"
 
 import Login from "../Pages/Login"
 import Cadastro from "../Pages/Cadastro";
